7b6588e7cbd9a01d6be9715d3dbc19d135d0575b
funke <arike406@gmail.com>
2022-04-27 12:56:10 +0100

    changes

---
+++ CyberAcademyTrainin -Join/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Linq;
+
+namespace CyberAcademyTrainin__Join
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string name = null;
+            var allstudent = Student.GetClasses();
+            var teacher = Standard.Getstandard();
+
+            //GroupJoin
+            //Query syntax
+            var data = from d in teacher
+                       join c in allstudent
+                       on d.id equals c.Standard into groupedteacher
+                       select new
+                       {
+                           d.id,
+                           d.ClassTeacher,
+                           groupedteacher
+                       };
+
+            var newdata = allstudent.Join(teacher,
+                                 b => b.Standard,
+                                 x => x.id, (bt, groupedteacher) => new
+                                 {
+                                     studentname = bt.Name,
+                                     Id = bt.Standard,
+                                     bt.Gender,
+                                     groupedteacher
+
+                                 });
+            foreach (var item in data)
+            {
+                Console.WriteLine($"name:{item.ClassTeacher}: id:{item.id}");
+                foreach (var itempro in item.groupedteacher)
+                {
+                    Console.WriteLine($" -------{itempro.Name} ------{itempro.RollNo}");
+                }
+            }
+            /** //Join
+             //Query syntax
+             var data = from b in allstudent
+                        join x in teacher
+                        on b.Standard equals x.id
+                        select new { 
+                            b.Name,
+                            x.ClassTeacher,
+                            x.id,
+                            b.Gender
+                        };
+             //Extension Method/BasedSyntax
+             var newdata = allstudent.Join(teacher, 
+                                 b => b.Standard, 
+                                 x => x.id, (bt, xl) => new
+             {
+                 studentname = bt.Name,
+                 teacher = xl.ClassTeacher,
+                 Id = bt.Standard,
+                 bt.Gender,
+                 xl.Fees
+             }).Where(b => b.Id > 6)
+                .OrderByDescending(x => x.Fees);
+
+             foreach (var alldata in newdata)
+             {
+                 Console.WriteLine($"StudentName  ----{alldata.studentname}: " +
+                                     $"Teacher:----{alldata.teacher} : Id -----{alldata.Id}:" +
+                                     $" SchoolLevy: ----{alldata.Fees}");
+             }
+            */
+
+
+
+        }
+    }
+}
---
+++ CyberAcademyTrainin -Join/Students.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CyberAcademyTrainin__Join
+{
+    public class Student
+    {
+        public int RollNo { get; set; }
+        public string Name { get; set; }
+        public int Standard { get; set; }
+        public Gender Gender { get; set; }
+        public static List<Student> GetClasses()
+        {
+            return new List<Student>
+            {
+               new Student{RollNo = 101, Name = "Thomas", Gender = Gender.Male, Standard = 10},
+               new Student{RollNo = 102, Name = "Chris", Gender = Gender.Male, Standard = 8},
+               new Student{RollNo = 103, Name = "Luise", Gender = Gender.Female, Standard = 10},
+               new Student{RollNo = 104, Name = "Ram", Gender = Gender.Male, Standard = 8},
+               new Student{RollNo = 105, Name = "Kate", Gender = Gender.Female, Standard = 6},
+               new Student{RollNo = 106, Name = "John", Gender = Gender.Male, Standard = 6},
+               new Student{RollNo = 107, Name = "Ema", Gender = Gender.Female, Standard = 6},
+               new Student{RollNo = 108, Name = "Ravi", Gender = Gender.Male}
+            };
+
+        }
+    }
+
+    public enum Gender
+    {
+        Female,
+        Male,
+    }
+
+    public class Standard
+    {
+        public int id { get; set; }
+        public string ClassTeacher { get; set; }
+        public double Fees { get; set; }
+
+        public static List<Standard> Getstandard()
+        {
+            return new List<Standard>
+            {
+                new Standard{id = 10, ClassTeacher="Max", Fees = 100.00},
+                new Standard{id = 8, ClassTeacher="Ajay", Fees = 800.00},
+                new Standard{id = 6, ClassTeacher="Geeta", Fees = 600.00},
+                new Standard{id = 4, ClassTeacher="Rohan", Fees = 400.00},
+            };
+
+        }
+    }
+}
